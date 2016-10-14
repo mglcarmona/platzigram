f9afb08b9949135c6b6e097e804cfb115081b863
Miguel Carmona Cantillo <mglcarmona@Miguels-MacBook-Air.local>
2016-10-14 16:04:54 -0500

    favicon added

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,17 @@ var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 
 gulp.task('styles', function() {
-  gulp.src('./index.scss')
+  gulp
+    .src('./index.scss')
     .pipe(sass())
     .pipe(rename('app.css'))
     .pipe(gulp.dest('./public'))
 });
 
-gulp.task('default', ['styles']);
+gulp.task('assets', function () {
+  gulp
+    .src('assets/*')
+    .pipe(gulp.dest('./public'))
+});
+
+gulp.task('default', ['styles', 'assets']);
