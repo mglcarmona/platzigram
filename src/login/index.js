78c086886cf8336323bc3b6a62bad9edaefe8608
Miguel Carmona <miguelrc1208@gmail.com>
2016-10-14 23:00:16 -0500

    ahora el title cambia dinamicamente con la vista

--- src/login/index.js
+++ src/login/index.js
@@ -1,8 +1,10 @@
 const page = require('page');
 const empty = require('empty-element');
 const template = require('./template.js');
+const title = require('title');
 
 page('/login', (ctx, next) => {
+	title('Platzigram - login');
   const main = document.getElementById('main-container');
   empty(main).appendChild(template);
 });
